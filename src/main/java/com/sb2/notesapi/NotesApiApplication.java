6c23ba2b278d149365592d8e4dc25d1edffece4c
Daniel Justino <danieljustino79@gmail.com>
2019-09-24 22:42:52 -0300

    Spring Boot 2, Spring Web, Spring Data MongoDB

---
+++ src/main/java/com/sb2/notesapi/NotesApiApplication.java
@@ -0,0 +1,15 @@
+package com.sb2.notesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NotesApiApplication.class, args);
+		System.out.println("SpringBoot 2");
+	}
+
+}
